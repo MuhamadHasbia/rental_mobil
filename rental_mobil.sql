9f989fad279454156ee1a8d54500b115202c32f9
MuhamadHasbia <147681900+MuhamadHasbia@users.noreply.github.com>
2023-12-24 22:28:15 +0700

    rental_mobil.sql

--- rental_mobil.sql
+++ rental_mobil.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 27, 2019 at 05:52 PM
--- Server version: 10.1.22-MariaDB
--- PHP Version: 7.1.4
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
